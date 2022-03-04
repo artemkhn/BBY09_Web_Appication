440aa6ae2c21f31141ba293cd33ebac625a94ebe
Artem Khan <akhan195@my.bcit.ca>
2022-03-04 12:26:11 -0800

    Added the sugestion cards.

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: "AIzaSyDZrPT38qIOT2_yC7pLb__1tnxxjvzp9vo",
-    authDomain: "bby09-59506.firebaseapp.com",
-    projectId: "bby09-59506",
-    storageBucket: "bby09-59506.appspot.com",
-    messagingSenderId: "98235674769",
-    appId: "1:98235674769:web:1316db00fcb4d6e858cf30"
+    apiKey: "AIzaSyAuptU8wCr8czy4FxL_SI_dZ2-IqPxkTBQ",
+    authDomain: "bby09-fe8f4.firebaseapp.com",
+    projectId: "bby09-fe8f4",
+    storageBucket: "bby09-fe8f4.appspot.com",
+    messagingSenderId: "825648357135",
+    appId: "1:825648357135:web:a3c07b73c198f5912b687a"
 };
 
 //--------------------------------------------
