b65b9ff32738e3e03ea0f23c8045cbe779519a35
Randall Kong <rkong18@my.bcit.ca>
2022-03-24 13:22:03 -0700

    make profile editable with phone number and username

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,29 +1,68 @@
-function writeCards() {
-    //define a variable for the collection you want to create in Firestore to populate data
-    var cardsRef = db.collection("mygems");
+var currentUser;
 
-    cardsRef.add({
-        name: "Kissa Tanto", //replace with your own city?
-        city: "Vancouver",
-        province: "BC",
-        patio: "Yes",
-        price: "$$$",
-        description: "Italian-Japanese cuisine & cocktails served in a chic space inspired by 1960's Tokyo jazz cafes."
+function populateProfile() {
+    firebase.auth().onAuthStateChanged(user => {
+
+        if (user) {
+
+            currentUser = db.collection("users").doc(user.uid)
+
+            currentUser.get()
+                .then(userDoc => {
+                    var userName = userDoc.data().name;
+                    var userUserName = userDoc.data().username;
+                    var userEmail = userDoc.data().email;
+                    var userPhone = userDoc.data().phone;
+                    
+                    
+                    if (userName != null) {
+                        document.getElementById("nameInput").value = userName;
+                    }
+                    
+                    if (userUserName != null) {
+                        document.getElementById("usernameInput").value = userUserName;
+                    }
+                    
+                    if (userEmail != null) {
+                        document.getElementById("emailInput").value = userEmail;
+                    }
+                    
+                    if (userPhone != null) {
+                        document.getElementById("phoneInput").value = userPhone;
+                    }
+                })
+        } else {
+            console.log("No user is signed in");
+        }
     });
-    cardsRef.add({
-        name: "Tacofino Taco Bar", //replace with your own city?
-        city: "Vancouver",
-        province: "BC",
-        price: "$",
-        description: "Casual, contemporary restaurant with West coast takes on classic Mexican food & a full bar."
-    });
-    cardsRef.add({
-        name: "Lunch Lady", //replace with your own city?
-        city: "Vancouver",
-        province: "BC",
-        price: "$$",
-        description: "Casual eatery offering Vietnamese staples such as pho, noodles & meat plates, plus unique cocktails."
-    });
+}
+
+populateProfile();
+
+
+function editUserProfile() {
+    //Enable the form fields
+    document.getElementById('profilefeilds').disabled = false;
+ }
+
+ function saveUserProfile() {
+    userName = document.getElementById('nameInput').value;
+    userUserName = document.getElementById('usernameInput').value;
+    userEmail = document.getElementById('emailInput').value;
+    userPhone = document.getElementById('phoneInput').value;
+
+    currentUser.update({
+            name: userName,
+            username: userUserName,
+            email: userEmail,
+            phone: userPhone
+        })
+        .then(() => {
+            console.log("Document successfully updated!");
+        })
+
+    //Enable the form fields
+    document.getElementById('profilefeilds').disabled = true;
 }
 
 function displayCards(collection) {
